4f7c3a87015beae812af7317b22e86532f365256
Max <max.style.dc@gmail.com>
2025-07-23 17:33:38 +0300

    hotfix 2

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,7 +1,7 @@
 import Agent from "@/components/Agent";
 import { getCurrentUser } from "@/lib/actions/auth.action";
 
-const page = async () => {
+const Page = async () => {
   const user = await getCurrentUser();
   return (
     <>
@@ -17,4 +17,4 @@ const page = async () => {
   );
 };
 
-export default page;
+export default Page;
